f5187d560e0dcfbf0f060cad25347de6141a7af5
Nutchanon Phongoen <nutchanon.pho@gmail.com>
2017-09-05 16:44:09 +0700

    Fix bug that incorrectly compute the children list of each node
    The bug is caused by passing the variable "list" which is inherited from the parent level to the recursion. The result will be duplicated values in nested children level. Instead, it should pass an empty list to the recursion.

--- elixir/lib/omise.ex
+++ elixir/lib/omise.ex
@@ -16,7 +16,8 @@ defmodule OMISE do
     inputString = ~s({"0":   [{"id": 10,    "title": "House",    "level": 0,    "children": [],    "parent_id": null}], "1":   [{"id": 12,    "title": "Red Roof",    "level": 1,    "children": [],    "parent_id": 10},   {"id": 18,    "title": "Blue Roof",    "level": 1,    "children": [],    "parent_id": 10},   {"id": 13,    "title": "Wall",    "level": 1,    "children": [],    "parent_id": 10}], "2":   [{"id": 17,    "title": "Blue Window",    "level": 2,    "children": [],    "parent_id": 12},   {"id": 16,    "title": "Door",    "level": 2,    "children": [],    "parent_id": 13},   {"id": 15,    "title": "Red Window",    "level": 2,    "children": [],    "parent_id": 12}]})
     jsonObject = Poison.Parser.parse!(inputString)
 
-    recordList = getRecordList(Map.values(jsonObject), [])
+    IO.inspect recordList = getRecordList(Map.values(jsonObject), []), pretty: true
+    # IO.inspect getResultList(recordList, recordList, []), pretty: true
     IO.inspect Poison.encode!(getResultList(recordList, recordList, []))
   end
 
@@ -29,8 +30,12 @@ defmodule OMISE do
   end
 
   def getChildrenWithThisParentId([head | tail], recordList, parent_id, list) do
+    IO.puts "parent_id #{parent_id} head_id #{head["id"]}"
     if head["parent_id"] == parent_id do
-      head = Map.put(head, "children", head["children"] ++ [getChildrenWithThisParentId(recordList, recordList, head["id"], list)])
+      IO.puts "childrenOfThisNode #{head["parent_id"]}"
+      IO.inspect childrenOfThisNode = getChildrenWithThisParentId(recordList, recordList, head["id"], [])
+      IO.puts "============"
+      head = Map.put(head, "children", head["children"] ++ childrenOfThisNode)
       list = list ++ [head]
     end
     getChildrenWithThisParentId(tail, recordList, parent_id, list)
@@ -42,7 +47,7 @@ defmodule OMISE do
 
   def getResultList([head | tail], recordList, list) do
     if head["parent_id"] == nil do
-      head = Map.put(head, "children", head["children"] ++ [getChildrenWithThisParentId(recordList, recordList, head["id"], list)])
+      head = Map.put(head, "children", head["children"] ++ getChildrenWithThisParentId(recordList, recordList, head["id"], list))
       list = list ++ [head]
     end
     getResultList(tail, recordList, list)
