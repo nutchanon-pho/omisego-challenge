066a4c38e9284cf97783744efea41ae3e70fd5b5
Nutchanon Phongoen <nutchanon.pho@gmail.com>
2017-09-05 16:45:07 +0700

    Remove debug log

--- elixir/lib/omise.ex
+++ elixir/lib/omise.ex
@@ -16,8 +16,8 @@ defmodule OMISE do
     inputString = ~s({"0":   [{"id": 10,    "title": "House",    "level": 0,    "children": [],    "parent_id": null}], "1":   [{"id": 12,    "title": "Red Roof",    "level": 1,    "children": [],    "parent_id": 10},   {"id": 18,    "title": "Blue Roof",    "level": 1,    "children": [],    "parent_id": 10},   {"id": 13,    "title": "Wall",    "level": 1,    "children": [],    "parent_id": 10}], "2":   [{"id": 17,    "title": "Blue Window",    "level": 2,    "children": [],    "parent_id": 12},   {"id": 16,    "title": "Door",    "level": 2,    "children": [],    "parent_id": 13},   {"id": 15,    "title": "Red Window",    "level": 2,    "children": [],    "parent_id": 12}]})
     jsonObject = Poison.Parser.parse!(inputString)
 
-    IO.inspect recordList = getRecordList(Map.values(jsonObject), []), pretty: true
-    # IO.inspect getResultList(recordList, recordList, []), pretty: true
+    recordList = getRecordList(Map.values(jsonObject), [])
+    getResultList(recordList, recordList, [])
     IO.inspect Poison.encode!(getResultList(recordList, recordList, []))
   end
 
@@ -30,11 +30,8 @@ defmodule OMISE do
   end
 
   def getChildrenWithThisParentId([head | tail], recordList, parent_id, list) do
-    IO.puts "parent_id #{parent_id} head_id #{head["id"]}"
     if head["parent_id"] == parent_id do
-      IO.puts "childrenOfThisNode #{head["parent_id"]}"
-      IO.inspect childrenOfThisNode = getChildrenWithThisParentId(recordList, recordList, head["id"], [])
-      IO.puts "============"
+      childrenOfThisNode = getChildrenWithThisParentId(recordList, recordList, head["id"], [])
       head = Map.put(head, "children", head["children"] ++ childrenOfThisNode)
       list = list ++ [head]
     end
