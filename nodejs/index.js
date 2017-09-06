1d83d91ec52f9c9a8d30f783858069a58846aba5
Nutchanon Phongoen <nutchanon.pho@gmail.com>
2017-09-06 21:36:25 +0700

    Receive input from command line arguments and improve output result printing

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,5 +1,9 @@
 let inputString = '{"0":   [{"id": 10,    "title": "House",    "level": 0,    "children": [],    "parent_id": null}], "1":   [{"id": 12,    "title": "Red Roof",    "level": 1,    "children": [],    "parent_id": 10},   {"id": 18,    "title": "Blue Roof",    "level": 1,    "children": [],    "parent_id": 10},   {"id": 13,    "title": "Wall",    "level": 1,    "children": [],    "parent_id": 10}], "2":   [{"id": 17,    "title": "Blue Window",    "level": 2,    "children": [],    "parent_id": 12},   {"id": 16,    "title": "Door",    "level": 2,    "children": [],    "parent_id": 13},   {"id": 15,    "title": "Red Window",    "level": 2,    "children": [],    "parent_id": 12}]}';
 
+if(process.argv[2]){
+    inputString = process.argv[2];
+}
+
 let jsonObject = JSON.parse(inputString);
 
 let objectMapById = {};
@@ -30,4 +34,8 @@ for (let key in objectMapById) {
         outputList.push(record);
     }
 }
-console.log(JSON.stringify(outputList, null, 1));
+
+console.log();
+console.log();
+console.log('Output Result:');
+console.log(JSON.stringify(outputList));
