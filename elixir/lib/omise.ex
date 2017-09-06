7372e5ed0be0342e19cb0423b625618c4470684a
Nutchanon Phongoen <nutchanon.pho@gmail.com>
2017-09-06 21:49:47 +0700

    Add the input from challenge git as the default input in case the user does not input anything

--- elixir/lib/omise.ex
+++ elixir/lib/omise.ex
@@ -1,7 +1,9 @@
 defmodule OMISE do
   def main do
     inputString = IO.gets "Input String:"
-    # inputString = ~s({"0":   [{"id": 10,    "title": "House",    "level": 0,    "children": [],    "parent_id": null}], "1":   [{"id": 12,    "title": "Red Roof",    "level": 1,    "children": [],    "parent_id": 10},   {"id": 18,    "title": "Blue Roof",    "level": 1,    "children": [],    "parent_id": 10},   {"id": 13,    "title": "Wall",    "level": 1,    "children": [],    "parent_id": 10}], "2":   [{"id": 17,    "title": "Blue Window",    "level": 2,    "children": [],    "parent_id": 12},   {"id": 16,    "title": "Door",    "level": 2,    "children": [],    "parent_id": 13},   {"id": 15,    "title": "Red Window",    "level": 2,    "children": [],    "parent_id": 12}]})
+    if String.length(inputString) == 1 do
+      inputString = ~s({"0":   [{"id": 10,    "title": "House",    "level": 0,    "children": [],    "parent_id": null}], "1":   [{"id": 12,    "title": "Red Roof",    "level": 1,    "children": [],    "parent_id": 10},   {"id": 18,    "title": "Blue Roof",    "level": 1,    "children": [],    "parent_id": 10},   {"id": 13,    "title": "Wall",    "level": 1,    "children": [],    "parent_id": 10}], "2":   [{"id": 17,    "title": "Blue Window",    "level": 2,    "children": [],    "parent_id": 12},   {"id": 16,    "title": "Door",    "level": 2,    "children": [],    "parent_id": 13},   {"id": 15,    "title": "Red Window",    "level": 2,    "children": [],    "parent_id": 12}]})
+    end
     jsonObject = Poison.Parser.parse!(inputString)
 
     recordList = getRecordList(Map.values(jsonObject), [])
